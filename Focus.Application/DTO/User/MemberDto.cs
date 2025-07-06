51d9a14c65cce920afed226f76110923ca569456
John <jonatascleytonf@gmail.com>
2025-07-06 14:35:32 -0300

    fix: made members return with ID

--- Focus.Application/DTO/User/MemberDto.cs
+++ Focus.Application/DTO/User/MemberDto.cs
@@ -2,12 +2,14 @@ namespace Focus.Application.DTO.User;
 
 public class MemberDto : SummaryUserDto
 {
+    public int Id { get; set; }
     public bool IsAdmin { get; set; }
     
     public static MemberDto FromUser(Domain.Entities.User user, bool isAdmin)
     {
         return new MemberDto
         {
+            Id = user.Id,
             ProfilePictureUrl = user.ProfilePictureUrl,
             Username = user.Username,
             Email = user.Email,
